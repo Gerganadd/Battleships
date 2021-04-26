ab7b2eee285f03f2d4a17a9750f04bb70997bc0b
admin-45053 <admin-45053@admin-45053-PC>
2021-04-26 16:09:41 +0300

    Class Ship

--- src/basic_classes/Ship.java
+++ src/basic_classes/Ship.java
@@ -2,5 +2,35 @@ package basic_classes;
 
 public class Ship
 {
-
+	private int length;
+	private boolean isSunk;
+	
+	public Ship(int length, boolean isSunk)
+	{
+		setLength(length);
+		isSunk = false;
+	}
+	
+	public int getLength()
+	{
+		return this.length;
+	}
+	
+	public boolean isSunk()
+	{
+		return this.isSunk;
+	}
+	
+	public void setLength(int length)
+	{
+		if(length >= 1 && length <= 5)
+		{
+			this.length  = length;
+		}
+		else
+		{
+			throw new IllegalArgumentException("Invalid value for length.");
+		}
+	}
+	
 }
