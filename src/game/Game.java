e5cf196ca0ed8a9155993178877dc299e9322de4
Gerganadd <gerganadoncheva2003@gmail.com>
2021-05-24 00:13:58 +0300

    created

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,51 @@
 package game;
 
-public class Game {
+import java.util.Random;
 
+import players.Player;
+import players.Robot;
+
+public class Game 
+{
+	public final static int MAX_POINTS = 24;
+	
+	private Player player;
+	private Robot bot;
+	
+	public Game()
+	{
+		this.player = new Player("player1");
+		this.bot = new Robot("bot");
+		
+		play();
+	}
+	
+	public static void main(String[] args)
+	{
+		Game newGame = new Game();
+	}
+	
+	private void play()
+	{
+		while(player.getPoints() != MAX_POINTS && bot.getPoints() != MAX_POINTS)
+		{
+			//player turn
+			//bot turn
+			
+			int row = randomGenerateNumber();
+			int col = randomGenerateNumber();
+			
+			//player.attack(row, col, bot.getBoard());
+			//bot.attack(player.getBoard());
+			System.out.println("player points : " + player.getPoints());
+			System.out.println("robot points : " + bot.getPoints());
+		}
+	}
+	
+	private static int randomGenerateNumber()
+	{
+		Random rand = new Random();
+		
+		return rand.nextInt(10); // to do - change it with Board.SIZE
+	}
 }
