bb8723eed49dcc367816942cfdb4461dc3e45765
Gerganadd <gerganadoncheva2003@gmail.com>
2021-05-24 00:13:05 +0300

    added base structure of Board

--- src/basic_classes/Board.java
+++ src/basic_classes/Board.java
@@ -2,5 +2,32 @@ package basic_classes;
 
 public class Board 
 {
-
+	public static final int SIZE = 10;
+	private Cell[][] playerBoard;
+	
+	public Board()
+	{
+		this.playerBoard = new Cell[SIZE][SIZE];
+		createBoard();
+	}
+	
+	private void createBoard()
+	{
+		
+	}
+	
+	public Cell[][] getPlayerBoard()
+	{
+		return this.playerBoard;
+	}
+	
+	public void addShip(int x, int y, Ship ship) 
+	{
+		
+	}
+	
+	public void printBoard()
+	{
+		
+	}
 }
