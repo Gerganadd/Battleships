b6901a8225a81b3e3d14364e14f483d099ca8571
admin-45053 <admin-45053@admin-45053-PC>
2021-06-12 18:01:28 +0300

    Started BeginningScreen to be continued

--- src/screens/BeginningScreen.java
+++ src/screens/BeginningScreen.java
@@ -1,5 +1,22 @@
 package screens;
 
-public class BeginningScreen {
+import java.awt.Color;
+
+public class BeginningScreen extends Screen
+{
+	public static final int FONT_SIZE = 20;
+	public static final int MAX_NAME_LENGTH = 20;
+	
+	public static final String DEFAULT_NAME = "Player";
+	
+	public static final Color BUTTON_COLOR = new Color(0, 153, 0);
+	
+
+	public BeginningScreen(String name) 
+	{
+		super(name);
+		
+	}
+	
 
 }
