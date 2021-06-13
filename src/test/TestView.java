25b49a5adf394005554b9d042f69d7c8872cc057
Gerganadd <gerganadoncheva2003@gmail.com>
2021-06-13 16:44:02 +0300

    changed TestView and SelectedShipView

--- src/test/TestView.java
+++ src/test/TestView.java
@@ -2,6 +2,8 @@ package test;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Label;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -12,17 +14,19 @@ import basic_classes.Cell;
 import players.BasePlayer;
 import players.Player;
 import players.Robot;
-import views.BoardView;
+//import views.BoardView;
 import views.CellView;
 import views.SelectShipView;
 
 public class TestView // go to GameViewScreen
 {
-
+	public static int pnlInformation_WIDTH = 10 * Cell.WIDTH;
+	public static int pnlInformation_HEIGHT = Cell.HEIGHT; // may be 30 or 40
+	
 	public static void main(String[] args) 
 	{
 		JFrame f = new JFrame();
-		f.setBounds(10, 10, 500, 500);
+		f.setBounds(10, 10, 800, 800);
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setLayout(new BoxLayout(f.getContentPane(), BoxLayout.X_AXIS));
@@ -34,41 +38,20 @@ public class TestView // go to GameViewScreen
 		spacePnl.setSize(new Dimension(50,50));
 		
 		JPanel shipsChoise = new SelectShipView();
-		//shipsChoise.setBackground(Color.green);
 		
-		f.add(playerBoard);
-		f.add(spacePnl);
 		f.add(shipsChoise);
+
+		f.add(playerBoard);
+		//f.add(spacePnl);
 		//f.add(robotBoard);
 		
 		f.setLocationRelativeTo(null);
 		f.pack();
 		f.repaint();
 		
-		
-		
-		//BoardView bw = new BoardView();
-		//f.add(bw);
-		//f.setLocationRelativeTo(null);
-		//f.pack();
-		//f.repaint();
 	}
 	
 	private static JPanel drawBasePlayerPnl(BasePlayer person)
-	{
-		JPanel toReturn = new JPanel();
-		toReturn.setLayout(new BoxLayout(toReturn, BoxLayout.Y_AXIS));
-		
-		//JPanel personalInformation = drawPnlOfPersonalInformation(person);
-		JPanel board = drawBoardOf(person);
-		
-		//toReturn.add(personalInformation);
-		toReturn.add(board);
-		
-		return toReturn;
-	}
-	
-	private static JPanel drawPnlOfPersonalInformation(BasePlayer person)
 	{
 		JPanel toReturn = new JPanel();
 		toReturn.setLayout(new BoxLayout(toReturn, BoxLayout.Y_AXIS));
@@ -80,6 +63,28 @@ public class TestView // go to GameViewScreen
 		toReturn.add(board);
 		
 		return toReturn;
+	}
+	
+
+	private static JPanel drawPnlOfPersonalInformation(BasePlayer person)
+	{
+		JPanel personalInformation = new JPanel();
+		
+		personalInformation.setSize(pnlInformation_WIDTH, pnlInformation_HEIGHT);
+		personalInformation.setPreferredSize(new Dimension(pnlInformation_WIDTH, pnlInformation_HEIGHT));
+		
+		personalInformation.setLayout(new BoxLayout(personalInformation, BoxLayout.Y_AXIS));
+		
+		Label name = new Label(person.getName());
+		name.setFont(new Font("Serif", Font.PLAIN, 17));
+		
+		Label points = new Label(String.valueOf(person.getPoints()));
+		points.setFont(new Font("Serif", Font.PLAIN, 17));
+		
+		personalInformation.add(name);
+		personalInformation.add(points);
+		
+		return personalInformation;
 	}
 	
 	private static JPanel drawBoardOf(BasePlayer person)
--- src/views/SelectShipView.java
+++ src/views/SelectShipView.java
@@ -1,14 +1,18 @@
 package views;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.util.LinkedList;
 
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 import basic_classes.Ship;
 
 public class SelectShipView extends JPanel
 {
+	
 	private LinkedList<ShipView> selectShipView;
 	private ShipView lastSelectedShip;
 	private int counter; //counts how many ships there are
@@ -58,9 +62,31 @@ public class SelectShipView extends JPanel
 	
 	public void paintComponent(Graphics graphics)
 	{
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setPnlSize();
+		
+		JPanel space = new JPanel();
+		space.setSize(20, 30);
+		space.setVisible(true);
+		
+		
 		for(ShipView s : selectShipView)
 		{
+			this.add(space);
 			this.add(s);
+			this.add(space);
 		}
+	}
+	
+	private void setPnlSize()
+	{
+		int WIDTH = 200;
+		int HEIGHT = 300;
+		
+		this.setSize(WIDTH, HEIGHT);
+		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		this.setMaximumSize(new Dimension(WIDTH, HEIGHT));
+		this.setMinimumSize(new Dimension(WIDTH, HEIGHT));
+						
 	}
 }
